78d35630f8242354fff0365a2131b1354cfd8042
Mohamed Nasr <104239430+mohnasr137@users.noreply.github.com>
2024-10-23 01:09:51 +0300

    add readers

--- routers/audio.js
+++ routers/audio.js
@@ -16,6 +16,20 @@ const reciterNamesInArabic = {
   "Mohamed al-Tablawi": "محمد الطبلاوي",
 };
 
+// "إبراهيم الأخضر",
+//   "أحمد بن علي العجمي",
+//   "أحمد نعينع",
+//   "أكرم العلاقمي",
+//   "سهل ياسين",
+//   "عبدالله بصفر",
+//   "عبدالله عواد الجهني",
+//   "عبدالمحسن القاسم",
+//   "علي بن عبدالرحمن الحذيفي",
+//   "علي حجاج السويسي",
+//   "عماد زهير حافظ",
+//   "خالد المهنا",
+//   "أحمد خليل شاهين";
+
 // routers
 audio.get("/by_surah", async (req, res) => {
   try {
@@ -90,7 +104,144 @@ audio.get("/recitations", async (req, res) => {
   }
 });
 
+audio.get("/timerReaders", async (req, res) => {
+  try {
+    const url = "https://mp3quran.net/api/v3/ayat_timing/reads";
+    const response = await axios.get(url);
+    if (response.status !== 200) {
+      return res.status(response.status).json({ message: "Failed to fetch" });
+    }
+
+    const recitations = response.data.map((recitation) => ({
+      id: recitation.id,
+      reciter_name: recitation.name,
+      rewaya: recitation.rewaya,
+    }));
+
+    return res.status(200).json({ recitations });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+audio.get("/timerReader", async (req, res) => {
+  try {
+    const { reader_id, surah_num } = req.query;
+    const url = `https://www.mp3quran.net/api/v3/ayat_timing?surah=${surah_num}&read=${reader_id}`;
+    const response = await axios.get(url);
+    if (response.status !== 200) {
+      return res.status(response.status).json({ message: "Failed to fetch" });
+    }
+
+    const timer = response.data.map((timer) => ({
+      ayah: timer.ayah,
+      polygon: timer.polygon,
+      start_time: timer.start_time,
+      end_time: timer.end_time,
+      x: timer.x,
+      y: timer.y,
+    }));
+
+    const newUrl = "https://mp3quran.net/api/v3/ayat_timing/reads";
+    const newResponse = await axios.get(newUrl);
+    if (newResponse.status !== 200) {
+      return res
+        .status(newResponse.status)
+        .json({ message: "Failed to fetch" });
+    }
+
+    let recitation = newResponse.data;
+    let length = recitation.length;
+    let reader = {};
+    for (let i = 0; i < length; i++) {
+      if (recitation[i].id == reader_id) {
+        reader = recitation[i];
+        break;
+      }
+    }
+
+    const paddedSurahNum = surah_num.toString().padStart(3, "0");
+    console.log(paddedSurahNum);
+    const folder_url = `${reader.folder_url}${paddedSurahNum}.mp3`;
+
+    reader = {
+      id: reader.id,
+      reciter_name: reader.name,
+      rewaya: reader.rewaya,
+      folder_url: folder_url,
+    };
+
+    return res.status(200).json({ reader, timer });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+audio.get("/readers", async (req, res) => {
+  try {
+    const url = "https://www.mp3quran.net/api/v3/reciters?language=ar";
+    const response = await axios.get(url);
+    if (response.status !== 200) {
+      return res.status(response.status).json({ message: "Failed to fetch" });
+    }
+
+    const recitations = response.data.reciters.map((recitation) => {
+      let length = recitation.moshaf.length;
+      let moshaf = [];
+      for (let i = 0; i < length; i++) {
+        moshaf.push({
+          id: recitation.moshaf[i].id,
+          name: recitation.moshaf[i].name,
+          surah_list: recitation.moshaf[i].surah_list,
+        });
+      }
+      return {
+        id: recitation.id,
+        reciter_name: recitation.name,
+        moshaf,
+      };
+    });
+
+    return res.status(200).json({ recitations });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+audio.get("/reader", async (req, res) => {
+  try {
+    const { reader_id, moshaf_id, surah_num } = req.query;
+    const url = `https://www.mp3quran.net/api/v3/reciters?language=ar&reciter=${reader_id}`;
+    const response = await axios.get(url);
+    if (response.status !== 200) {
+      return res.status(response.status).json({ message: "Failed to fetch" });
+    }
+
+    let recitation = response.data.reciters[0];
+    let length = recitation.moshaf.length;
+    let moshaf = {};
+    for (let i = 0; i < length; i++) {
+      if (recitation.moshaf[i].id == moshaf_id) {
+        moshaf = recitation.moshaf[i];
+        break;
+      }
+    }
+
+    const paddedSurahNum = surah_num.toString().padStart(3, "0");
+    console.log(paddedSurahNum);
+    const newUrl = `${moshaf.server}${paddedSurahNum}.mp3`;
+    recitation = {
+      reciter_name: recitation.name,
+      moshaf_name: moshaf.name,
+      surah_num,
+      audio_url: newUrl,
+    };
+
+    return res.status(200).json({ recitation });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
 // exports
 module.exports = audio;
-
-
